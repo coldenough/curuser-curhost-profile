70eab9527404b5288b77a1d3a04b3b298bac27be
coldenough <vrednopolezno@gmail.com>
2015-05-25 12:10:55 -0700

    Added npp alias

--- Profile.ps1
+++ Profile.ps1
@@ -3,6 +3,14 @@ File location:
 cp ./Profile.ps1 C:\Users\dkoldunov\Documents\WindowsPowerShell
 #>
 
+# PSDrives
 New-PSDrive -Name Git -PSProvider FileSystem `
     -Root 'C:\Users\dkoldunov\Documents' `
     -Description 'dkoldunov@ drive'
+
+# Aliases
+$nppAliasPath = "C:\Program Files (x86)\Notepad++\notepad++.exe"
+if (Test-Path $nppAliasPath) {
+  Set-Alias -Name npp -Value $nppAliasPath -Description 'dkoldunov@ aliases' `
+      -Option ReadOnly
+} #end of if 
