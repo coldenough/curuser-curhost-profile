7279da433646fd666b49eea2bf53b40a1a2241ef
coldenough <vrednopolezno@gmail.com>
2015-05-25 11:54:53 -0700

    Added PSdrive Git:

--- Profile.ps1
+++ Profile.ps1
@@ -2,3 +2,7 @@
 File location:
 C:\Users\dkoldunov\Documents\WindowsPowerShell
 #>
+
+New-PSDrive -Name Git -PSProvider FileSystem `
+    -Root 'C:\Users\dkoldunov\Documents' `
+    -Description 'dkoldunov@ drive'
