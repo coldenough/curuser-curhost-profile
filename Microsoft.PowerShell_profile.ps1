947d7ebd7a5bbecc0f52b9ff319cd90737989b7c
coldenough <vrednopolezno@gmail.com>
2015-05-25 18:20:50 -0700

    Fixed path to profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,7 @@
 <#
 File location:
 cp ./Microsoft.PowerShell_profile.ps1 `
-    C:\Users\dkoldunov\Documents\Microsoft.PowerShell_profile.ps1
+    C:\Users\dkoldunov\Documents\WidowsPowerShell\Microsoft.PowerShell_profile.ps1
 #>
 
 # Reassign $HOME directory
